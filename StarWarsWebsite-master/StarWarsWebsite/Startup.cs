dffceabf1e448d66d832942de0ca93906772a479
finneganspencer <spencerfinnegan@gmail.com>
2016-10-27 15:10:52 -0600

    initial commit

---
+++ StarWarsWebsite-master/StarWarsWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StarWarsWebsite.Startup))]
+namespace StarWarsWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
